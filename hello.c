f6eaa915ed56f832a1b7e5943ca1b29d7e4605fd
apgautham <gauthamplayer@gmail.com>
2017-09-20 18:15:23 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+
+int a;
+scanf("%d",a);
+if(a<0)
+printf("negative");
+else if(a==0)
+printf("zero");
+else
+printf("negative");
+return 0;
+}
